7f1a1d7cf327e54c245207d6c59300cfcbc606d8
PEDRO NUNES MOREIRA <pmoreira@cs.up.edu.br>
2026-03-16 19:55:21 -0300

    Commit inicial

---
+++ Solucao/Ex01/Program.cs
@@ -0,0 +1,43 @@
+﻿int tmnVetor = 1000;
+int maxValor = 100;
+Random random = new Random();
+bool ordenado = false;
+int[] vetor = new int[tmnVetor];
+
+Console.WriteLine("\nDesordenados:");
+
+for (int i = 0; i < tmnVetor; i++)
+{
+    vetor[i] = random.Next(maxValor) + 1;
+    Console.Write(vetor[i] + " ");
+}
+
+while (!ordenado)
+{
+    for (int i = 0; i < tmnVetor; i++)
+    {
+        if (i < tmnVetor - 1 && vetor[i] > vetor[i + 1])
+        {
+            int aux = vetor[i];
+            vetor[i] = vetor[i + 1];
+            vetor[i + 1] = aux;
+        }
+    }
+
+    for (int i = 0; i < tmnVetor; i++)
+    {
+        ordenado = true;
+        if (i < tmnVetor - 1 && vetor[i] > vetor[i + 1])
+        {
+            ordenado = false;
+            break;
+        }
+    }
+}
+
+Console.WriteLine("\n\nOrdenados:");
+
+for (int i = 0; i < tmnVetor; i++)
+{
+    Console.Write(vetor[i] + " ");
+}
