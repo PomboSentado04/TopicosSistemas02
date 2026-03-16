a9da3492ed66285e50073f6216394e699d4bde45
PEDRO NUNES MOREIRA <pmoreira@cs.up.edu.br>
2026-03-16 20:11:42 -0300

    Atualizado código

--- Solucao/Ex01/Program.cs
+++ Solucao/Ex01/Program.cs
@@ -1,4 +1,5 @@
-﻿int tmnVetor = 1000;
+﻿// Variáveis
+int tmnVetor = 1000;
 int maxValor = 100;
 Random random = new Random();
 bool ordenado = false;
@@ -6,14 +7,33 @@ int[] vetor = new int[tmnVetor];
 
 Console.WriteLine("\nDesordenados:");
 
+// Coloca valores no vetor e imprime vetor desordenado
 for (int i = 0; i < tmnVetor; i++)
 {
     vetor[i] = random.Next(maxValor) + 1;
     Console.Write(vetor[i] + " ");
 }
 
+Console.WriteLine("\n");
+
+// Imprime quantidade de vezes que cada valor apareceu: vetor desordenado
+for (int i = 0; i < maxValor + 1; i++)
+{
+    int quantidadeValor = 0;
+    for (int j = 0; j < tmnVetor; j++)
+    {
+        if (i == vetor[j])
+        {
+            quantidadeValor++;
+        }
+    }
+    Console.WriteLine(i + ": " + quantidadeValor);
+}
+
+// Sai do loop apenas se vetor estiver ordenado
 while (!ordenado)
 {
+    // Troca os valores se a posição atual do vetor for maior que a posterior
     for (int i = 0; i < tmnVetor; i++)
     {
         if (i < tmnVetor - 1 && vetor[i] > vetor[i + 1])
@@ -24,6 +44,7 @@ while (!ordenado)
         }
     }
 
+    // Verifica se o vetor está ordenado corretamente
     for (int i = 0; i < tmnVetor; i++)
     {
         ordenado = true;
@@ -37,7 +58,24 @@ while (!ordenado)
 
 Console.WriteLine("\n\nOrdenados:");
 
+// Imprime vetor ordenado
 for (int i = 0; i < tmnVetor; i++)
 {
     Console.Write(vetor[i] + " ");
+}
+
+Console.WriteLine("\n");
+
+// Imprime quantidade de vezes que cada valor apareceu: vetor ordenado
+for (int i = 0; i < maxValor + 1; i++)
+{
+    int quantidadeValor = 0;
+    for (int j = 0; j < tmnVetor; j++)
+    {
+        if (i == vetor[j])
+        {
+            quantidadeValor++;
+        }
+    }
+    Console.WriteLine(i + ": " + quantidadeValor);
 }
