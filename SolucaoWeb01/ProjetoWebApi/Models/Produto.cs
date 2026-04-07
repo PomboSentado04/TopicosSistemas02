28aebf21a30db7033956fadda5e407357b642bfd
PEDRO NUNES MOREIRA <pmoreira@cs.up.edu.br>
2026-04-06 21:42:11 -0300

    Nova aula blz.

---
+++ SolucaoWeb01/ProjetoWebApi/Models/Produto.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace ProjetoWebApi.Models;
+
+public class Produto
+{
+    public string? Nome { get; set; }
+}
--- SolucaoWeb01/ProjetoWebApi/Program.cs
+++ SolucaoWeb01/ProjetoWebApi/Program.cs
@@ -1,10 +1,36 @@
+using ProjetoWebApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+List<Produto> listaDeProdutos = new()
+{
+    new Produto { Nome = "Notebook Gamer" },
+    new Produto { Nome = "Mouse Sem Fio" },
+    new Produto { Nome = "Teclado Mecânico" },
+    new Produto { Nome = "Monitor 4K" },
+    new Produto { Nome = "Headset Bluetooth" },
+    new Produto { Nome = "Webcam HD" },
+    new Produto { Nome = "Cadeira Ergonômica" },
+    new Produto { Nome = "Suporte para Monitor" },
+    new Produto { Nome = "Microfone Condensador" },
+    new Produto { Nome = "SSD 1TB" }
+};
+
 // Endpoints - Funcionalidades aplicação
 app.MapGet("/", () => "API de Produtos!");
-app.MapGet("/funcionalidade", () => "API de Produtos DOS!");
-app.MapPost("/funcionalidade", () => "API de Produtos POST!");
+
+// GET: /api/produto/listar
+app.MapGet("/api/produto/listar", () =>
+{
+    return listaDeProdutos;
+});
+
+// POST: /api/produto/cadastrar
+app.MapPost("/api/produto/cadastrar", (Produto produto) =>
+{
+    listaDeProdutos.Add(produto);
+});
 
 // Rodar aplicação
 app.Run();
